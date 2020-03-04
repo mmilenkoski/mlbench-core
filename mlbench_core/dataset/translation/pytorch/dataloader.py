18c94c9dd797f3e7a35119a6fc50097f8384dafb
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2020-03-04 15:53:08 +0100

    Add tokenizer for WMT14

--- mlbench_core/dataset/translation/pytorch/dataloader.py
+++ mlbench_core/dataset/translation/pytorch/dataloader.py
@@ -1,27 +1,22 @@
 import os
-
+import torch
 import torchtext
 import torchtext.datasets as nlp_datasets
-
-PAD_WORD = "<blank>"
-UNK_WORD = "<unk>"
-BOS_WORD = "<s>"
-EOS_WORD = "</s>"
-EXTS = (".en", ".de")
+from mlbench_core.dataset.translation.pytorch import config, Tokenizer
 
 
 def _get_nmt_text(batch_first):
     tokenizer = "spacy"
     SRC_TEXT = torchtext.data.Field(
         tokenize=torchtext.data.utils.get_tokenizer(tokenizer, language="en"),
-        pad_token=PAD_WORD,
+        pad_token=config.PAD_TOKEN,
         batch_first=batch_first,
     )
     TGT_TEXT = torchtext.data.Field(
         tokenize=torchtext.data.utils.get_tokenizer(tokenizer, language="de"),
-        init_token=BOS_WORD,
-        eos_token=EOS_WORD,
-        pad_token=PAD_WORD,
+        init_token=config.BOS_TOKEN,
+        eos_token=config.EOS_TOKEN,
+        pad_token=config.PAD_TOKEN,
         batch_first=batch_first,
     )
     return SRC_TEXT, TGT_TEXT
@@ -41,6 +36,7 @@ class WMT14Dataset(nlp_datasets.WMT14):
     def __init__(
         self,
         root,
+        tokenizer=None,
         download=True,
         train=True,
         fields=None,
@@ -60,15 +56,28 @@ class WMT14Dataset(nlp_datasets.WMT14):
         else:
             path = os.path.join(root, "wmt14/wmt14")
 
+        self.tokenizer = tokenizer
+        if self.tokenizer is None:
+            self.tokenizer = Tokenizer(os.path.join(path, config.VOCAB_FNAME))
+
         if train:
-            path = os.path.join(path, "train.tok.clean.bpe.32000")
+            path = os.path.join(path, config.TRAIN_FNAME)
         else:
-            path = os.path.join(path, "newstest2013.tok.bpe.32000")
+            path = os.path.join(path, config.VAL_FNAME)
 
         filter_pred = lambda x: not (
             len(vars(x)["src"]) > max_sent_length
             or len(vars(x)["trg"]) > max_sent_length
         )
         super(WMT14Dataset, self).__init__(
-            path=path, fields=self.fields, exts=EXTS, filter_pred=filter_pred
+            path=path, fields=self.fields, exts=config.EXTS, filter_pred=filter_pred
         )
+
+    def __getitem__(self, idx):
+        example = super().__getitem__(idx)
+        src, tgt = example.src, example.trg
+
+        src = torch.tensor(self.tokenizer.segment(src))
+        tgt = torch.tensor(self.tokenizer.segment(tgt))
+
+        return src, tgt
