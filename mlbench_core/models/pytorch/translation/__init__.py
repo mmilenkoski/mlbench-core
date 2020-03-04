d1e9471be806c19b4e4a7f9f5ed785f3447fb78c
Edoardo Hölzl <edoardo.holzl@epfl.ch>
2020-03-04 17:42:04 +0100

    Add GNMT model

---
+++ mlbench_core/models/pytorch/translation/__init__.py
@@ -0,0 +1,5 @@
+from .attention import BahdanauAttention
+from .utils import init_lstm_
+from .models import GNMT
+from .decoder import ResidualRecurrentDecoder
+from .encoder import ResidualRecurrentEncoder
